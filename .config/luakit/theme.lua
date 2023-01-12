60f54f37689181f6a10189203db6a2a96e1c3f65
Zoh-j02r <saulobruno@usp.br>
2023-01-12 01:41:11 -0300

    feat(luakit): initial configuration directory

---
+++ .config/luakit/theme.lua
@@ -0,0 +1,93 @@
+local c = require('palette')
+local theme = {}
+
+-- Default settings
+theme.font = "16px Iosevka Nerd Font"
+theme.fg = c.fg0
+theme.bg = c.bg0
+
+-- Genaral colours
+theme.success_fg = c.gr1
+theme.loaded_fg = c.bu0
+theme.error_fg = c.bg1
+theme.error_bg = c.rd0
+
+-- Warning colours
+theme.warning_fg = c.bg1
+theme.warning_bg = c.yw1
+
+-- Notification colours
+theme.notif_fg = c.bg1
+theme.notif_bg = c.lg1
+-- Menu colours
+theme.menu_fg = c.fg0  
+theme.menu_bg = c.bg1  
+theme.menu_selected_fg = c.wh1 
+theme.menu_selected_bg = c.bg3 
+theme.menu_title_bg = c.bl1
+theme.menu_primary_title_fg = c.wh0
+theme.menu_secondary_title_fg = c.wh1
+
+theme.menu_disabled_fg = c.fg2
+theme.menu_disabled_bg = c.bg1
+theme.menu_enabled_fg = c.fg0
+theme.menu_enabled_bg = c.bg1
+theme.menu_active_fg = c.cy1
+theme.menu_active_bg = c.bg1
+
+-- Proxy manager
+theme.proxy_active_menu_fg = c.fg1
+theme.proxy_active_menu_bg = c.bg1
+theme.proxy_inactive_menu_fg = c.wh1
+theme.proxy_inactive_menu_bg = c.bg1
+
+-- Statusbar specific
+theme.sbar_fg = c.fg0
+theme.sbar_bg = c.bg2
+
+-- Downloadbar specific
+theme.dbar_fg = c.cy0
+theme.dbar_bg = c.bg1
+theme.dbar_error_fg = c.rd0
+
+-- Input bar specific
+theme.ibar_fg = c.fg0
+theme.ibar_bg = c.bg2
+
+-- Tab label
+theme.tab_fg = c.fg0
+theme.tab_bg = c.bg2
+theme.tab_hover_bg = c.bg1
+theme.tab_ntheme = c.bl1
+theme.selected_fg = c.wh0
+theme.selected_bg = c.bg0
+theme.selected_ntheme = c.bl1
+theme.loading_fg = c.bu0
+theme.loading_bg = c.bg0
+
+theme.selected_private_tab_bg = c.hfg
+theme.private_tab_bg    = c.bl1
+
+-- Trusted/untrusted ssl colours
+theme.trust_fg = c.gr0
+theme.notrust_fg = c.rd0
+
+-- Follow mode hints
+theme.hint_font = "10px monospace"
+theme.hint_fg = c.fg0
+theme.hint_bg = c.bg1
+theme.hint_border = "0px"
+theme.hint_opacity = "0.3"
+theme.hint_overlay_bg = c.bg3
+theme.hint_overlay_border = "0px"
+theme.hint_overlay_selected_bg = c.bu1
+theme.hint_overlay_selected_border = theme.hint_overlay_border
+
+-- General colour pairings
+theme.ok = { fg = c.gr0, bg = c.bg1 }
+theme.warn = { fg = c.yw0, bg = c.bg1 }
+theme.error = { fg = c.rd0, bg = c.bg1 }
+
+return theme
+
+-- vim: et:sw=4:ts=8:sts=4:tw=80
---
+++ .config/luakit/userconf.lua
@@ -0,0 +1,178 @@
+local c = require('palette')
+local chrome = require('chrome')
+local downloads = require('downloads')
+
+downloads.default_dir = os.getenv("HOME") .. "/Download"
+
+	-- SOLVE: Find a way to store this string as a html file
+	-- SOLVE: reduce the amounts of !important's
+	-- WHAT A MESS
+chrome.stylesheet = [===[
+    * {
+        box-sizing: border-box;
+        border: 0px !important;
+        border-radius: 0px !important;
+        box-shadow: 0px 0px !important; 
+    }
+    body {
+        background-color: ]===] .. c.bg0 .. [===[ !important;
+        color: ]===] .. c.fg0 .. [===[ !important;
+        display: block;
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+    }
+    #page-header {
+        display: flex;
+        -webkit-align-items: center;
+        background-color: ]===] .. c.bg0 .. [===[;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        margin: 0;
+        padding: 0 1.5em;
+        height: 3.5em;
+        -webkit-user-select: none;
+        overflow-y: hidden;
+        z-index: 100000;
+    }
+    #page-header > h1 {
+        font-size: 1.4rem;
+        margin: 0 1em;
+        color: #445;
+        cursor: default;
+    }
+    #page-header > h1:first-child {
+        margin-left: 0;
+    }
+    .content-margin {
+        padding: 3.5em 1.5em 0 1.5em;
+    }
+    h2 { font-size: 1.2rem; }
+    h3 { font-size: 1.1rem; color: #666; }
+
+    #page-header input, .setting input, #edit-dialog input, #edit-dialog textarea {
+        font-size: 0.8rem;
+        padding: 0.5rem 0.75rem;
+        border: none;
+        outline: none;
+        margin-top: 0;
+        margin-bottom: 0;
+    	background-color: ]===] .. c.bg3 .. [===[;
+    	color: lightgrey;
+    }
+
+    #page-header #search-box {
+        display: flex;
+        padding: 0;
+        background-color: #fff;
+    }
+
+    #page-header #search {
+        width: 20em;
+        font-weight: normal;
+        margin: 0;
+        padding-right: 0;
+    }
+
+    #page-header #clear-button {
+        margin: 0;
+        padding: 0.5rem 0.55rem;
+        font-size: 1rem;
+        line-height: 1rem;
+    }
+
+    #page-header #clear-button:hover {
+        color: #000;
+    }
+
+    #page-header #clear-button:active {
+    	background-color: ]===] .. c.bg3 .. [===[;
+    }
+
+    .button {
+        margin: 1rem 0 1rem 0.5rem;
+        color: #888;
+        display: inline-block;
+        line-height: 1.25;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -webkit-user-select: none;
+        padding: .5rem 1rem;
+        font-size: 1rem;
+        transition: color .1s ease-in-out, background-color .1s ease-in-out;
+        cursor: pointer;
+    }
+
+    #page-header .button:hover, .button:hover, #edit-dialog input[type="button"], #edit-dialog input[type="submit"] {
+        color: #fff !important;
+    }
+
+    #page-header .button:active, .button:active {
+    	background-color: ]===] .. c.bg3 .. [===[;
+    }
+
+    #page-header .button[disabled], .button[disabled] {
+        color: #888;
+	background-color: transparent;
+        cursor: not-allowed;
+    }
+
+    #page-header .rhs {
+        display: flex;
+        -webkit-align-items: center;
+        position: fixed;
+        top: 0;
+        right: 0;
+        margin: 0;
+        padding-right: 1.5em;
+        height: 3.5em;
+        margin: 0;
+        background-color: inherit;
+    }
+
+    #page-header .rhs .button {
+        margin-bottom: 0;
+    }
+
+    .license {
+        font-family: monospace;
+    }
+
+    .hidden {
+        display: none;
+    }
+    h3.mode-name, .bookmark .controls a:hover {
+	-webkit-box-shadow: 0px 0px !important;
+	background-color: transparent !important;
+        color: ]===] .. c.fg0 .. [===[ !important;
+    }
+    .disable {
+    	color: ]===] .. c.fg2 .. [===[;
+    }
+    .state_Enabled, .boolean > span[data-value=true]  {
+	color: ]===] .. c.fg2 .. [===[ !important;
+    }
+    .state_Disabled, .boolean > span[data-value=false] {
+        color: ]===] .. c.bl0 .. [===[ !important;
+    }
+    .bind .link-box a, .bind code, .title a, .item a, a:any-link {
+	color: ]===] .. c.fg2 .. [===[ !important;
+	text-decoration: none; 
+    }
+    #settings, .setting:not(.disabled), .bind pre, #blackout {
+        background: transparent !important;
+    }
+    #edit-dialog input, #edit-dialog textarea {
+	background-color: ]===] .. c.bg2 .. [===[ !important;
+    }
+    .setting:hover, .bind:hover, .item.selected, #edit-dialog {
+        background: ]===] .. c.bg1 .. [===[ !important;
+    }
+    .setting.disabled {
+        background: ]===] .. c.bl0  .. [===[ !important;
+        cursor: not-allowed;
+    }
+]===]
