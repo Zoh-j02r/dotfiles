08e7d635ff20677686570c6dcfea8882c7cd745d
Zoh-j02r <saulobruno@usp.br>
2023-07-25 17:03:53 -0300

    feat: colors/spacing +utilities changes

--- .config/luakit/theme.lua
+++ .config/luakit/theme.lua
@@ -2,30 +2,31 @@ local c = require('palette')
 local theme = {}
 
 -- Default settings
-theme.font = "15px Source Code Pro"
-theme.fg = c.fg0
+theme.font = "14px Source Code Pro"
+theme.fg = c.wh1
 theme.bg = c.bg0
 
 -- Genaral colours
 theme.success_fg = c.gr1
 theme.loaded_fg = c.bu0
-theme.error_fg = c.bg1
-theme.error_bg = c.rd0
+theme.error_fg = c.bg0
+theme.error_bg = c.rd1
 
 -- Warning colours
-theme.warning_fg = c.bg1
+theme.warning_fg = c.bg0
 theme.warning_bg = c.yw1
 
 -- Notification colours
-theme.notif_fg = c.bg1
-theme.notif_bg = c.lg1
+theme.notif_fg = c.wh1
+theme.notif_bg = c.bl0
+
 -- Menu colours
-theme.menu_fg = c.fg0  
-theme.menu_bg = c.bg1  
-theme.menu_selected_fg = c.wh1 
-theme.menu_selected_bg = c.bg3 
+theme.menu_fg = c.fg0
+theme.menu_bg = c.bg0
+theme.menu_selected_fg = c.wh1
+theme.menu_selected_bg = c.bl1
 theme.menu_title_bg = c.bl1
-theme.menu_primary_title_fg = c.wh0
+theme.menu_primary_title_fg = c.wh1
 theme.menu_secondary_title_fg = c.wh1
 
 theme.menu_disabled_fg = c.fg2
@@ -42,8 +43,8 @@ theme.proxy_inactive_menu_fg = c.wh1
 theme.proxy_inactive_menu_bg = c.bg1
 
 -- Statusbar specific
-theme.sbar_fg = c.fg0
-theme.sbar_bg = c.bg2
+theme.sbar_fg = c.wh1
+theme.sbar_bg = c.bl1
 
 -- Downloadbar specific
 theme.dbar_fg = c.cy0
@@ -51,22 +52,21 @@ theme.dbar_bg = c.bg1
 theme.dbar_error_fg = c.rd0
 
 -- Input bar specific
-theme.ibar_fg = c.fg0
-theme.ibar_bg = c.bg2
+theme.ibar_fg = c.wh1
+theme.ibar_bg = c.bl1
 
 -- Tab label
-theme.tab_fg = c.fg0
-theme.tab_bg = c.bg2
-theme.tab_hover_bg = c.bg2
-theme.tab_ntheme = c.bl1
-theme.selected_fg = c.wh0
-theme.selected_bg = c.bg0
+theme.tab_fg = c.wh1
+theme.tab_bg = c.bl1
+theme.tab_hover_bg = c.bg1
+theme.tab_ntheme = c.bl0
+theme.selected_fg = c.wh1
+theme.selected_bg = c.bl0
 theme.selected_ntheme = c.fg1
-theme.loading_fg = c.bu0
+theme.loading_fg = c.fg0
 theme.loading_bg = c.bg0
-
-theme.selected_private_tab_bg = c.fg1
-theme.private_tab_bg    = c.fg2
+theme.selected_private_tab_bg = c.bg3
+theme.private_tab_bg = c.bg2
 
 -- Trusted/untrusted ssl colours
 theme.trust_fg = c.gr0
